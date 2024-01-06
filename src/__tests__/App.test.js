2409d0dda0cd9fbebbdb4e0a3a8befe1a168f26d
Brett <brett_neighbors@yahoo.com>
2024-01-06 16:28:12 -0700

    Inputted new tests for each component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,14 @@
-import {render} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 describe("App", () => {
-    test("renders without crashing", () => {
-        render(<App />)
-    });
+  test("renders without crashing", () => {
+    render(<App />);
+  });
+
+  test("renders three Welcome back messages", () => {
+    render(<App />);
+    const messages = screen.getAllByText(/welcome back/i);
+    expect(messages).toHaveLength(3);
+  });
 });
